78cc696feab422c651235052efb495a0b340a5c6
Manuel-Alejandro-Cortes <manuel_ale.cortes@uao.edu.co>
2025-10-19 22:52:42 -0500

    Agrega logica basica minijuego 2

---
+++ Project_SMA/Assets/Player/Scripts/Scripts2Minijuego/TouchRotate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        //!GameControl.youWin
+        if (!false)
+        {
+            transform.Rotate(0f,0f, 90f);
+        }
+    }
+}
